06b2b9729f9fdd3fb3db226d6190e6e9123dc94f
Dimas186 <63162718+Dimas186@users.noreply.github.com>
2020-04-05 22:58:09 +0300

    0.0.6

--- app/src/main/java/com/example/transportcontrol/AddingActivityModer0.java
+++ app/src/main/java/com/example/transportcontrol/AddingActivityModer0.java
@@ -3,12 +3,22 @@ package com.example.transportcontrol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddingActivityModer0 extends AppCompatActivity {
+
+    EditText motorcade, vehicleType, brand, plateNumber, inventoryNumber, garageNumber, drivers, technicalInspection;
+    EditText insurance, firstAidKit, extinguisher, previousTechnicalInspection, comments, coolantDensity;
+    EditText electricalEquipmentState, sufficientPressureInTheFireExtinguisher, electrolyteDensity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_moder0);
+    }
+
+    public void onClick(View v) {
+
     }
 }
