762016140e03882842aed1bbff0bef334ca269db
Dimas186 <63162718+Dimas186@users.noreply.github.com>
2020-04-05 22:21:42 +0300

    0.0.4

--- app/src/main/java/com/example/transportcontrol/MainActivity.java
+++ app/src/main/java/com/example/transportcontrol/MainActivity.java
@@ -163,6 +163,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     public void onClick(View v) {
-        startActivity(new Intent(this, AddingActivityModer0.class));
+        startActivity(new Intent(this, AddingActivityModer12.class));
     }
 }
