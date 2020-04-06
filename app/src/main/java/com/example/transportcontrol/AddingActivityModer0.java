5d2234de8d44c7d5739cd11ce125a9f77639902b
Dimas186 <63162718+Dimas186@users.noreply.github.com>
2020-04-06 11:54:10 +0300

    added ocr

--- app/src/main/java/com/example/transportcontrol/AddingActivityModer0.java
+++ app/src/main/java/com/example/transportcontrol/AddingActivityModer0.java
@@ -1,16 +1,32 @@
 package com.example.transportcontrol;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.transportcontrol.model.DataModel;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
 
 public class AddingActivityModer0 extends AppCompatActivity {
 
@@ -46,6 +62,10 @@ public class AddingActivityModer0 extends AppCompatActivity {
         switch (v.getId()) {
             case R.id.addDriver:
                 driversList.add(drivers.getText().toString());
+                drivers.setText("");
+                break;
+            case R.id.scan:
+                showImageImportDialog();
                 break;
             case R.id.btnAdd:
                 setDataModel();
@@ -93,5 +113,55 @@ public class AddingActivityModer0 extends AppCompatActivity {
         dataModel.setElectricalEquipmentState(electricalEquipmentState.getText().toString());
         dataModel.setSufficientPressureInTheFireExtinguisher(sufficientPressureInTheFireExtinguisher.getText().toString());
         dataModel.setElectrolyteDensity(electrolyteDensity.getText().toString());
+    }
+
+    private void showImageImportDialog() {
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+        startActivityForResult(intent, 1213);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1213) {
+                String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+                CropImage.activity(Uri.fromFile(new File(filePath)))
+                        .setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                if (!recognizer.isOperational()) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < items.size(); i++) {
+                        TextBlock myitem = items.valueAt(i);
+                        sb.append(myitem.getValue());
+                        sb.append("\n");
+                    }
+                    plateNumber.setText(sb.toString());
+                }
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
